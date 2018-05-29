7721eb037b2c5aff3837850bb8d3e2943c561b05
Peter McGill <pmg7670@gmail.com>
2018-05-29 15:00:38 +1000

    Update __init__.py

--- apibase/__init__.py
+++ apibase/__init__.py
@@ -1,21 +1,30 @@
 from appProvider import AppProvider
+import logging
 
 appPrvdr = AppProvider()
-appPrvdr.init('/home/workspace/leveldb/devdb1')
+appPrvdr.init('/apps/home/u352425/emltnApi')
+#appPrvdr.init('/home/workspace/leveldb/devdb1')
 
-def runJob(jobId, *argv):
+def dispatch(jobId, *argv, **kwargs):
   logger = logging.getLogger('apscheduler')
   try:
     delegate = appPrvdr._job[jobId]
   except KeyError:
     logger.error('jobId not found in job register : ' + jobId)
     return
-
-  delegate(argv)
+  
+  delegate(*argv, **kwargs)
+  try:
+    delegate.state
+    delegate.listener
+  except AttributeError:
+    # only stateful jobs are retained
+    with appPrvdr.lock:
+      del(appPrvdr._job[jobId])
+    return
   if delegate.state.complete:
+    logger.info('director[%s] is complete, removing it now ...', delegate.state.jobId)
     with appPrvdr.lock:
       if hasattr(delegate, 'listener'):
         appPrvdr.scheduler.remove_listener(delegate.listener)
       del(appPrvdr._job[jobId])
-
-  
