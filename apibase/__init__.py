c4a3c15ceec9235bc5f1fce4fb77eaf299647e8b
Peter McGill <pmg7670@gmail.com>
2018-06-11 11:19:55 +1000

    Update __init__.py

--- apibase/__init__.py
+++ apibase/__init__.py
@@ -1,12 +1,12 @@
 from appProvider import AppProvider
 import logging
 
-logger = logging.getLogger('apscheduler')
 appPrvdr = AppProvider()
 appPrvdr.init('/apps/home/u352425/emltnApi')
 #appPrvdr.init('/home/workspace/leveldb/devdb1')
 
 def dispatch(jobId, *argv, **kwargs):
+  logger = logging.getLogger('apscheduler')
   try:
     delegate = appPrvdr._job[jobId]
   except KeyError:
@@ -19,12 +19,26 @@ def dispatch(jobId, *argv, **kwargs):
     delegate.listener
   except AttributeError:
     # only stateful jobs are retained
+    try:
+      if delegate.removeMeta:
+        removeMeta(jobId)
+    except AttributeError:
+      pass
     with appPrvdr.lock:
       del(appPrvdr._job[jobId])
     return
   if delegate.state.complete:
-    logger.info('director[%s] is complete, removing it now ...', delegate.state.jobId)
+    logtxt = 'director[%s] is complete, removing it now ...'
+    if delegate.runMode == 'FAILED':
+      logtxt = 'director[%s] has failed, removing it now ...'
+    logger.info(logtxt, delegate.state.jobId)
+    removeMeta(jobId)
     with appPrvdr.lock:
       if hasattr(delegate, 'listener'):
         appPrvdr.scheduler.remove_listener(delegate.listener)
       del(appPrvdr._job[jobId])
+
+def removeMeta(jobId):
+
+  dbKey = 'PMETA|' + jobId
+  appPrvdr.db.Delete(dbKey)
