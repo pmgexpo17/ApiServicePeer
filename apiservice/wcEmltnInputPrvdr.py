cd06b0717d9158a0961b54727c93a8d8f20dac40
Peter McGill <pmg7670@gmail.com>
2018-06-17 17:31:13 +1000

    Update wcEmltnInputPrvdr.py

--- apiservice/wcEmltnInputPrvdr.py
+++ apiservice/wcEmltnInputPrvdr.py
@@ -3,163 +3,54 @@ import logging
 import json
 import requests, re
 from apiservice import AppDelegate, logger
-from subprocess import Popen, PIPE
 
 # -------------------------------------------------------------- #
 # WcEmltnInputPrvdr
 # ---------------------------------------------------------------#
-class WcEmltnInputPrvdr(AppDelegate):
+class WcEmltnInputPrvdr(AppDirector):
 
   def __init__(self, leveldb, jobId):
-    super(WcEmltnInputPrvdr,self).__init__(leveldb, jobId=jobId)
-    self.pmeta = None
+    super(WcEmltnInputPrvdr,self).__init__(leveldb, jobId)
+    self.appType = 'delegate'
+    self.state.hasNext = True
+    self.resolve = WcResolveUnit(leveldb)
+    self.resolve.state = self.state
 
-	# -------------------------------------------------------------- #
-	# run
-	# ---------------------------------------------------------------#
-  def __call__(self):
-
-    try:
-      self.runApp()
-    except Exception as ex:
-      self.putApiRequest(500)
-      
-	# -------------------------------------------------------------- #
-	# run
-	# ---------------------------------------------------------------#
-  def runApp(self):
-
-    self.loadProgramMeta()
-    self.getPmovDataFromS3()    
-    self.landriveMount()
-    self.checkInputFile()
-    self.compileSessionVars('wcInputXml2Sas.sas')
-    sasPrgm = '%s/wcInputXml2Sas.sas' % self.pmeta['progLib']
-    logfile = '%s/log/wcInputXml2Sas.log' % self.pmeta['progLib']
-    sysArgs = ['sas','-sysin',sasPrgm,'-log',logfile,'-logparm','open=replace']
-
-    logger.info('run wcInputXml2Sas.sas in subprocess ...')
-    self.runProcess(sysArgs)
-    self.putApiRequest(201)
-
-	# -------------------------------------------------------------- #
-	# convertXmlToSas
-	# ---------------------------------------------------------------#
-  def convertXmlToSas(self):
-
-    env = Environment(loader=PackageLoader('apiservice', 'xmlToSas'),trim_blocks=True)
-    sasPrgm = '%s/get.sas' % self.pmeta['progLib']
-    logfile = '%s/log/wcInputXml2Sas.log' % self.pmeta['progLib']
-    sysArgs = ['sas','-sysin',sasPrgm,'-log',logfile,'-logparm','open=replace']
-    
-	# -------------------------------------------------------------- #
-	# getPmovDataFromS3
-	# ---------------------------------------------------------------#
-  def getPmovDataFromS3(self):
-
-    pmovDir = 'pmov'
-    if self.pmeta['ciwork'][-1] != '/':
-      pmovDir = '/pmov'
-    pmovLib = self.pmeta['ciwork'] + pmovDir
-    self.pmeta['pmovLib'] = pmovLib
-    
-    if self.pmeta["inputTxnFilter"] != 'LAST_INF_BY_PCID':
-      return
-    try:      
-      pmovDsId = datetime.datetime.strptime(self.pmeta['pmovDsId'],'%y%m')
-    except ValueError:
-      raise Exception('pmov dataset id not valid : ' + self.pmeta['pmovDsId'])
-
-    if not os.path.exists(pmovLib):
-      self.runProcess(['mkdir','-p',pmovLib])
+  # -------------------------------------------------------------- #
+  # _start
+  # ---------------------------------------------------------------#                                          
+  def _start(self):
+    _pmeta = self.getProgramMeta()
+    pmeta = _pmeta['WcInputXmlToSas']
+    _globals = pmeta['globals'] # append global vars in this list
+    del(pmeta['globals'])
+    if _globals[0] == '*':
+      pmeta.update(_pmeta['Global'])
     else:
-      pmovDsName = 'pmov%s_combined.sas7bdat' % self.pmeta['pmovDsId']
-      pmovDsPath = '%s/%s' % (pmovLib, pmovDsName)
-      if os.path.exists(pmovDsPath):        
-        lastPutStamp = os.path.getmtime(pmovDsPath)
-        lastPut = datetime.datetime.fromtimestamp(lastPutStamp)
-        today = datetime.datetime.today()
-        if lastPut.day == today.day and lastPut.month == today.month and lastPut.year == today.year:
-          logger.info(pmovDsName + ' has been transfered already today')
-          logger.info('Note : wcEmltnService S3 transfer min period = daily')
-          return
+      for item in _globals:
+        pmeta[item] = _pmeta['Global'][item]    
+    self.resolve.pmeta = pmeta
+    self.tsXref = self.resolve._start()
 
-    pmovS3Dir = pmovDsId.strftime('%Y%m')
-    if self.pmeta['pmovS3Base'][-1] != '/':
-      pmovS3Dir = '/' + pmovS3Dir
-    self.pmeta['pmovS3Repo'] += pmovS3Dir
-    incItems = ['macroLib','pmovLib','pmovS3Repo','pmovDsId']
-    self.compileSessionVars('getPmovFromS3.sas',incItems=incItems)
-    
-    sasPrgm = '%s/getPmovFromS3.sas' % self.pmeta['progLib']
-    logfile = '%s/log/getPmovFromS3.log' % self.pmeta['progLib']
-    sysArgs = ['sas','-sysin',sasPrgm,'-log',logfile,'-logparm','open=replace']
-        
-    logger.info('run getPmovFromS3.sas in subprocess ...')
-    self.runProcess(sysArgs)
 
   # -------------------------------------------------------------- #
-  # landriveMount
+  # advance
   # ---------------------------------------------------------------#                                          
-  def landriveMount(self):
-    
-    logger.info('[START] landriveMount')
-    self.mountPath = 'webapi/wcemltn/session'
-    logger.info('landrive mount path : ' + self.mountPath)
-    unMounted = self.runProcess(['grep','-w',self.mountPath,'/etc/mtab'],returnRc=True)
-    if not unMounted:
-      logger.info('landrive is already mounted : ' + self.mountPath)
-      return
-
-    logger.info('wcemltn service base : ' + self.pmeta['localBase'])
-    cifsEnv = os.environ['HOME'] + '/.wcemltn/cifs_env'
-    with open(cifsEnv,'w') as fhw:
-      fhw.write('ADUSER=%s\n' % os.environ['USER'])
-      fhw.write("DFSPATH='%s'\n" % self.pmeta['localBase'])
-      fhw.write('MOUNTINST=%s\n' % self.mountPath)
-    
-    credentials = os.environ['HOME'] + '/.landrive'
-    runDir = os.environ['HOME'] + '/.wcemltn'
-    sysArgs = ['sudo','landrive.sh','--mountcifs','--cifsenv','cifs_env','--credentials',credentials]
-    self.runProcess(sysArgs,cwd=runDir)
-
-    sysArgs = ['grep','-w',self.mountPath,'/etc/mtab']
-    unMounted = self.runProcess(['grep','-w',self.mountPath,'/etc/mtab'],returnRc=True)
-    if unMounted:
-      errmsg = 'failed to mount landrive : ' + self.mountPath
-      logger.error(errmsg)
-      raise Exception(errmsg)
+  def advance(self, signal=None):
+    if self.state.transition == 'NORMALISE_XML':
+      # signal = the http status code of the companion promote method
+      if signal == 201:
+        logger.info('state transition is resolved, advancing ...')
+        self.state.transition = 'NA'
+        self.state.inTransition = False
+      else:
+        raise Exception('WcEmltnInputPrvdr server process failed, rc : %d' % signal)
 
   # -------------------------------------------------------------- #
-  # checkInputFile
+  # getProgramMeta
   # ---------------------------------------------------------------#                                          
-  def checkInputFile(self):
-    logger.info('[START] checkInputFile')
-
-    linkPath = self.pmeta['linkBase'] + '/session'
-    logger.info('landrive symlink : ' + linkPath)
-    self.landrive = '/lan/%s/%s' % (os.environ['USER'], self.mountPath)
-    logger.info('landrive : ' + self.landrive)
-    if not os.path.exists(linkPath):
-      self.runProcess(['ln','-s', self.landrive, linkPath])
-    self.pmeta['inputXmlBase'] = linkPath + '/inputXml'
-
-    inputXmlPath = self.pmeta['inputXmlBase'] + '/' + self.pmeta['inputXmlFile']
-    try:
-      self.runProcess(['ls', inputXmlPath])
-    except Exception:
-      errmsg = 'wcEmulation inputXmlFile is not found : ' +  inputXmlPath
-      logger.error(errmsg)
-      raise Exception(errmsg)
-    else:
-      logger.info('wcEmulation inputXmlFile : ' + inputXmlPath)
-    logger.info('[END] checkInputFile')
-    
-  # -------------------------------------------------------------- #
-  # loadProgramMeta
-  # ---------------------------------------------------------------#                                          
-  def loadProgramMeta(self):
-    logger.debug('[START] loadProgramMeta')
+  def getProgramMeta(self):
+    logger.debug('[START] getProgramMeta')
     dbKey = 'PMETA|' + self.jobId
     try:
       _pmeta = self._leveldb.Get(dbKey)
@@ -177,6 +68,197 @@ class WcEmltnInputPrvdr(AppDelegate):
       for item in _globals:
         self.pmeta[item] = pmeta['Global'][item]
 
+  # -------------------------------------------------------------- #
+  # onComplete
+  # ---------------------------------------------------------------#
+  def onComplete(self):
+    self.putApiRequest(201)
+
+  # -------------------------------------------------------------- #
+  # onError
+  # ---------------------------------------------------------------#
+  def onError(self, errorMsg):
+    self.putApiRequest(500)
+
+  # -------------------------------------------------------------- #
+  # quicken
+  # ---------------------------------------------------------------#
+  def quicken(self):      
+    logger.info('state transition : ' + self.state.transition)
+    if self.state.transition == 'NORMALISE_XML':
+      self.resolve.putApiRequest()
+
+  # -------------------------------------------------------------- #
+  # putApiRequest
+  # ---------------------------------------------------------------#
+  def putApiRequest(self, signal):
+    if self.state.transition == 'STREAM_TO_SAS':
+      classRef = 'xmlToStream:XmlToStream'
+      pdata = (self.state.jobId,classRef)
+      params = '{"type":"director","id":"%s","responder":"self","service":"%s","args":[]}' % pdata
+      data = [('job',params)]
+      apiUrl = 'http://localhost:5000/api/v1/job/1'
+      response = requests.post(apiUrl,data=data)
+      logger.info('api response ' + response.text)
+    elif self.state.complete:
+      classRef = 'wcEmltnService:WcEmltnDirector'
+      pdata = (self.jobId,classRef,json.dumps({'signal':signal}))
+      params = '{"type":"director","id":"%s","service":"%s","kwargs":%s,"args":[]}' % pdata
+      data = [('job',params)]
+      apiUrl = 'http://localhost:5000/api/v1/job/1'
+      response = requests.post(apiUrl,data=data)
+      logger.info('api response ' + response.text)
+
+# -------------------------------------------------------------- #
+# WcResolveUnit
+# ---------------------------------------------------------------#
+class WcResolveUnit(AppResolveUnit):
+  def __init__(self, leveldb):
+    self._leveldb = leveldb
+    self.__dict__['NORMALISE_XML'] = self.NORMALISE_XML
+    self.__dict__['IMPORT_TO_SAS'] = self.IMPORT_TO_SAS
+    self.__dict__['GET_PMOV_DATA'] = self.GET_PMOV_DATA
+    self.__dict__['TRANSFORM_SAS'] = self.TRANSFORM_SAS
+    self.pmeta = None
+
+  # -------------------------------------------------------------- #
+  # _start
+  # ---------------------------------------------------------------#
+  def _start(self):
+    self.loadProgramMeta()
+    dbKey = 'CRED|' + self.jobId
+    credentials = self._leveldb.Get(dbKey)
+    stashPath = '/%s/%s' % (self.pmeta['stashHost'],self.pmeta['stashBase'])
+    uriPath = '%s/%s/%s?raw' % (stashPath,self.pmeta['schemaRepo'],self.pmeta['xmlSchema'])
+    dbKey = 'TSXREF|' + self.jobId
+    self.tsXref = self._leveldb.Get(dbKey)
+    xmlSchema = '%s/%s/%s' %  (self.pmeta['ciwork'],self.tsXref,self.pmeta['xmlSchema'])
+    self.runProcess(['curl',credentials,uriPath,'-o',xmlSchema])
+    self.importXmlSchema(xmlSchema)
+    return self.tsXref
+    
+  # -------------------------------------------------------------- #
+  # importXmlSchema
+  # ---------------------------------------------------------------#
+  def importXmlSchema(self, xmlSchema):
+    
+    with open(xmlSchema,'r') as fhr:
+      schemaDoc = json.load(fhr)
+    tableDefn = schemaDoc['tableDefn']
+    columnOrder = schemaDoc['columnOrder']
+    columnDefn = schemaDoc['columnDefn']
+
+    sasDefn = {}
+    for tableName in sorted(tableDefn):
+      _sasDefn = []
+      for nodeKey in columnOrder[tableName]
+        for defnItem in columnDefn[nodeKey]:
+          del(defnItem[1])
+          _sasDefn += self.getSasFormat(defnItem)
+      sasDefn[tableName] = _sasDefn
+    self.sasDefn = sasDefn
+
+  # -------------------------------------------------------------- #
+  # getSasFormat
+  # ---------------------------------------------------------------#
+  def getSasFormat(self, fmtItem):
+    fmtItem += '$' if fmtItem[1][0] == '$' else ''
+    return [fmtItem]
+    
+	# -------------------------------------------------------------- #
+	# NORMALISE_XML
+	# ---------------------------------------------------------------#
+  def NORMALISE_XML(self):
+    self.state.transition = 'NORMALISE_XML'
+    self.state.inTransition = True
+    self.state.next = 'IMPORT_TO_SAS'
+    self.state.hasNext = True
+    return self.state
+
+	# -------------------------------------------------------------- #
+	# IMPORT_TO_SAS
+	# ---------------------------------------------------------------#
+  def IMPORT_TO_SAS(self):
+    env = Environment(loader=PackageLoader('apiservice', 'xmlToSas'),trim_blocks=True)
+    template = env.get_template('streamToSas.jinja')    
+    for tableName in sorted(self.clmnDefn):
+      sasDefn = self.clmnDefn[tableName]
+      sasPrgm = '%s/%s/%s.sas' % (self.pmeta['ciwork'],self.tsXref,tableName)
+      template.stream(jobId=self.jobId,tableName=tableName,sasDefn=sasDefn).dump(sasPrgm)
+
+      logfile = '%s/%s/log/%s.log' % (self.pmeta['ciwork'],self.tsXref,tableName)
+      sysArgs = ['sas','-sysin',sasPrgm,'-log',logfile,'-logparm','open=replace']
+      self.runProcess(sysArgs)
+    self.state.next = 'GET_PMOV_DATA'
+    self.state.hasNext = True
+    return self.state
+
+	# -------------------------------------------------------------- #
+	# GET_PMOV_DATA
+	# ---------------------------------------------------------------#
+  def GET_PMOV_DATA(self):
+
+    self.state.next = 'TRANSFORM_SAS'
+    self.state.hasNext = True
+    if self.pmeta["inputTxnFilter"] != 'LAST_INF_BY_PCID':
+      return self.state
+
+    pmovDir = 'pmov'
+    if self.pmeta['ciwork'][-1] != '/':
+      pmovDir = '/pmov'
+    pmovLib = self.pmeta['ciwork'] + pmovDir
+    self.pmeta['pmovLib'] = pmovLib
+
+    try:      
+      pmovDsId = datetime.datetime.strptime(self.pmeta['pmovDsId'],'%y%m')
+    except ValueError:
+      raise Exception('pmov dataset id not valid : ' + self.pmeta['pmovDsId'])
+
+    if not os.path.exists(pmovLib):
+      self.runProcess(['mkdir','-p',pmovLib])
+    else:
+      pmovDsName = 'pmov%s_combined.sas7bdat' % self.pmeta['pmovDsId']
+      pmovDsPath = '%s/%s' % (pmovLib, pmovDsName)
+      if os.path.exists(pmovDsPath):        
+        lastPutStamp = os.path.getmtime(pmovDsPath)
+        lastPut = datetime.datetime.fromtimestamp(lastPutStamp)
+        today = datetime.datetime.today()
+        if lastPut.day == today.day and lastPut.month == today.month and lastPut.year == today.year:
+          logger.info(pmovDsName + ' has been transfered already today')
+          logger.info('Note : wcEmltnService S3 transfer min period = daily')
+          return self.state
+
+    pmovS3Dir = pmovDsId.strftime('%Y%m')
+    if self.pmeta['pmovS3Base'][-1] != '/':
+      pmovS3Dir = '/' + pmovS3Dir
+    self.pmeta['pmovS3Repo'] += pmovS3Dir
+    incItems = ['macroLib','pmovLib','pmovS3Repo','pmovDsId']
+    self.compileSessionVars('getPmovFromS3.sas',incItems=incItems)
+    
+    sasPrgm = '%s/getPmovFromS3.sas' % self.pmeta['progLib']
+    logfile = '%s/log/getPmovFromS3.log' % self.pmeta['progLib']
+    sysArgs = ['sas','-sysin',sasPrgm,'-log',logfile,'-logparm','open=replace']
+        
+    logger.info('run getPmovFromS3.sas in subprocess ...')
+    self.runProcess(sysArgs)
+    return self.state
+
+	# -------------------------------------------------------------- #
+	# TRANSFORM_SAS
+	# ---------------------------------------------------------------#
+  def TRANSFORM_SAS(self):
+
+    self.compileSessionVars('wcInputXml2Sas.sas')
+    sasPrgm = '%s/wcInputXml2Sas.sas' % self.pmeta['progLib']
+    logfile = '%s/log/wcInputXml2Sas.log' % self.pmeta['progLib']
+    sysArgs = ['sas','-sysin',sasPrgm,'-log',logfile,'-logparm','open=replace']
+
+    logger.info('run wcInputXml2Sas.sas in subprocess ...')
+    self.runProcess(sysArgs)
+    self.state.hasNext = False
+    self.state.complete = True
+
+    
   # -------------------------------------------------------------- #
   # compileSessionVars
   # ---------------------------------------------------------------#
@@ -209,14 +291,3 @@ class WcEmltnInputPrvdr(AppDelegate):
       if not incItems or itemKey in incItems:
         fhw.write(puttext.format(itemKey, self.pmeta[itemKey]))
 
-  # -------------------------------------------------------------- #
-  # putApiRequest
-  # ---------------------------------------------------------------#
-  def putApiRequest(self, signal):
-    classRef = 'wcEmltnService:WcEmltnDirector'
-    pdata = (self.jobId,classRef,json.dumps({'signal':signal}))
-    params = '{"type":"director","id":"%s","service":"%s","kwargs":%s,"args":[]}' % pdata
-    data = [('job',params)]
-    apiUrl = 'http://localhost:5000/api/v1/job/1'
-    response = requests.post(apiUrl,data=data)
-    logger.info('api response ' + response.text)
