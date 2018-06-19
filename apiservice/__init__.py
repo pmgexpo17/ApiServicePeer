60ac63afd5b3dc46f4e02ca30245bb8573ad5a1c
Peter McGill <pmg7670@gmail.com>
2018-06-19 19:02:34 +1000

    Update __init__.py

--- apiservice/__init__.py
+++ apiservice/__init__.py
@@ -12,4 +12,4 @@
 #  See the License for the specific language governing permissions and
 #  limitations under the License. 
 #
-from appDirector import AppDirector, AppState, AppResolveUnit, AppListener, AppDelegate, StreamPrvdr, SasScriptPrvdr, logger
+from appDirector import SubProcHandler, AppDirector, AppState, AppResolveUnit, AppListener, StreamPrvdr, SasScriptPrvdr, logger
