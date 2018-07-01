4ab6b75b6831cedd45254e501bdd6f32250bf6b9
Peter McGill <pmg7670@gmail.com>
2018-07-02 08:25:58 +1000

    Update appDirector.py

--- apiservice/appDirector.py
+++ apiservice/appDirector.py
@@ -1,16 +1,24 @@
+# The MIT License
+#
 # Copyright (c) 2018 Peter A McGill
 #
-#  Licensed under the Apache License, Version 2.0 (the "License");
-#  you may not use this file except in compliance with the License.
-#  You may obtain a copy of the License at
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
 #
-#    http://www.apache.org/licenses/LICENSE-2.0
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
 #
-#  Unless required by applicable law or agreed to in writing, software
-#  distributed under the License is distributed on an "AS IS" BASIS,
-#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-#  See the License for the specific language governing permissions and
-#  limitations under the License. 
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+# THE SOFTWARE.
 #
 from abc import ABCMeta, abstractmethod
 from threading import RLock
@@ -77,8 +85,11 @@ class AppDirector(SysCmdUnit):
     self.jobId = jobId
     self.state = AppState(jobId)
     self.resolve = None
-        
-  # run wrkcmp emulation by apScheduler
+    self.hasPeerSignal = False
+
+  # ------------------------------------------------------------ #
+  # generic actor method
+  # -------------------------------------------------------------#
   def __call__(self, *argv, **kwargs):
     with self.state.lock:
       if self.state.status == 'STOPPED':
@@ -120,6 +131,8 @@ class AppDirector(SysCmdUnit):
           break  
         state = self.advance()
         logger.info('next state : ' + self.state.current)
+      if self.hasPeerSignal:
+        self.quicken()
     except Exception as ex:
       #self.mailer[state.current]('ERROR')
       self.onError(str(ex))
@@ -219,30 +232,36 @@ class AppListener(object):
     pass
 
 # -------------------------------------------------------------- #
-# StreamPrvdr
+# MetaReader
 # ---------------------------------------------------------------#
-class StreamPrvdr(object):
+class MetaReader():
   __metaclass__ = ABCMeta
 
-  @abstractmethod
-  def renderStream(self, *args, **kwargs):
-    pass
-
-# -------------------------------------------------------------- #
-# SasScriptPrvdr
-# ---------------------------------------------------------------#
-class SasScriptPrvdr(SysCmdUnit):
-  __metaclass__ = ABCMeta
-
-  def __init__(self):
+  def __init__(self, leveldb):
+    self._leveldb = leveldb
     self.pmeta = None
-    
+
+  # -------------------------------------------------------------- #
+  # restoreMeta
+  # ---------------------------------------------------------------#
+  def restoreMeta(self, dbKey):
+    try:
+      pmetadoc = self._leveldb.Get(dbKey)
+    except KeyError:
+      raise Exception('EEOWW! pmeta json document not found : ' + dbKey)
+    return json.loads(pmetadoc)
+        
   # -------------------------------------------------------------- #
   # getProgramMeta
   # ---------------------------------------------------------------#
   @abstractmethod
   def getProgramMeta(self):
     pass
+
+# -------------------------------------------------------------- #
+# SasScriptPrvdr
+# ---------------------------------------------------------------#
+class SasScriptPrvdr(MetaReader):
 
   # -------------------------------------------------------------- #
   # compileScript
