a062053a8f8c5287c89fb321ffd137e22ac7ec47
Peter McGill <gilletahom5@gmail.com>
2019-11-24 12:51:28 +1000

    Update clientB.py

--- app/apiservice/j190306100703/clientB.py
+++ app/apiservice/j190306100703/clientB.py
@@ -2,50 +2,48 @@
 #
 # Copyright (c) 2018 Peter A McGill
 #
-from apibase import AppDirector, ApiRequest, AppResolvar, JobMeta
-from apiservice.j190306100703 import promote, iterate
+from apibase import AppCooperator, TaskError
+from . import promote, iterate
 import logging
 
-logger = logging.getLogger('apipeer.smart')
+logger = logging.getLogger('asyncio.smart')
 
 # -------------------------------------------------------------- #
 # ClientB
 # ---------------------------------------------------------------#
-class ClientB(AppDirector):
-  def __init__(self, leveldb, actorId):
-    super().__init__(leveldb, actorId)
-    self._type = 'director'
+class ClientB(AppCooperator):
+  def __init__(self, actorId):
+    super().__init__(actorId)
     self.state.hasNext = True
-    self.resolve = Resolvar(leveldb)
-    self.resolve.state = self.state
-    self.request = ApiRequest()
+    self.resolve = None
 
   # -------------------------------------------------------------- #
   # __getitem__
   # ---------------------------------------------------------------#
   def __getitem__(self, key):
+    if key in self._quicken:
+      return self._quicken[key]
     if key in self.__dict__:
       return self.__dict__[key]
-    elif key in self._quicken:
-      return self._quicken[key]
-    raise TypeError(f'{self.name}, {key} does not exist in obj properties')
+    raise TypeError(f'{key} is not a valid {self.name} attribute')
 
   # -------------------------------------------------------------- #
   # start
   # ---------------------------------------------------------------#
   @promote('clientB')
   def start(self, jobId, jobMeta, **kwargs):
-    logger.info(f'{self.name}.start ...')
+    logger.info(f'{self.name} is starting ...')
     self.jobId = jobId
-    self.hostName = jobMeta['hostName']
+    self.resolve = Resolvar()
+    self.resolve.state = self.state    
     self.resolve.start(jobId, jobMeta)
      
   # -------------------------------------------------------------- #
   # onError
   # ---------------------------------------------------------------#
   def onError(self, ex):
-    actorId, actorName = self.tell()
     state = self.state
+    actorId, actorName = self.tell()
     if state.status != 'STARTED':
       logMsg = f'actor error, job was {state.status}, aborting ...'
     else:
@@ -54,21 +52,29 @@ class ClientB(AppDirector):
     logMsg = f'{logMsg}\nActor, {actorName}, {actorId},'
     logger.error(logMsg, exc_info=True)
 
+  # -------------------------------------------------------------- #
+  # destroy
+  # -------------------------------------------------------------- #
+  def destroy(self, *args, **kwargs):
+    pass
+
+  # -------------------------------------------------------------- #
+  # stop
+  # -------------------------------------------------------------- #
+  def stop(self, *args, **kwargs):
+    pass
+
 # The MIT License
 #
 # Copyright (c) 2018 Peter A McGill
 #
-import simplejson as json
-import os, sys
+from apibase import json, AppResolvar, Note, TaskError
+import os
 
 # -------------------------------------------------------------- #
 # Resolvar
 # ---------------------------------------------------------------#
 class Resolvar(AppResolvar):
-
-  def __init__(self, leveldb):
-    self._leveldb = leveldb
-    self.request = ApiRequest()
 
   # -------------------------------------------------------------- #
   # start
@@ -85,81 +91,43 @@ class Resolvar(AppResolvar):
   # -------------------------------------------------------------- #  
   @iterate('clientB')
   def DOWNLOAD_ZIPFILE(self):
-    self.downloadZipfile()
-    self.uncompressFile()
+    self.downloadJsonFile()
 
   # -------------------------------------------------------------- #
-  # downloadZipfile
+  # FINAL_HANDSHAKE
+  # -------------------------------------------------------------- #  
+  @iterate('clientB')
+  def FINAL_HANDSHAKE(self):
+    pass
+
+  # -------------------------------------------------------------- #
+  # downloadJsonFile
   # ---------------------------------------------------------------#
-  def downloadZipfile(self):
-    jpacket = {'metaKey':'repoMeta','typeKey':'REPO'}
-    repoMeta = self.runQuery(JobMeta(jpacket))
+  def downloadJsonFile(self):
+    # the itemKey context is opposite, to check if the applied category 
+    # exists in registered consumerCategories
+    jpacket = {'eventKey':f'REPO|{self.jobId}','itemKey':'jsonToCsv'}
+    repo = self.query(Note(jpacket))
 
-    if not os.path.exists(repoMeta['sysPath']):
-      errmsg = 'xform output path does not exist : ' + repoMeta['sysPath']
-      raise Exception(errmsg)
+    apiBase = self._leveldb['apiBase']
+    sysPath = f'{apiBase}/{repo.sysPath}'
+    if not os.path.exists(sysPath):
+      errmsg = f'output repo path does not exist : {sysPath}'
+      raise TaskError(errmsg)
 
-    catPath = self.jmeta['category']
-    if catPath not in repoMeta['consumer categories']:
-      errmsg = 'consumer category branch %s does not exist under : %s ' \
-                                          % (catPath, repoMeta['sysPath'])
-      raise Exception(errmsg)
+    catPath = self.jmeta.category
+    if catPath not in repo.consumerCategories:
+      errmsg = 'consumer category branch %s does not exist in %s' \
+                                % (catPath, str(repo.consumerCategories))
+      raise TaskError(errmsg)
   
-    self.repoPath = '%s/%s' % (repoMeta['sysPath'], catPath)
-    logger.info('output json gzipfile repo path : ' + self.repoPath)
+    repoPath = f'{sysPath}/{catPath}'
+    logger.info('output json gzipfile repo path : ' + repoPath)
 
-    self.jsonZipFile = '%s.%s' % (self.jobId, self.jmeta['fileExt'])
-    logger.info('output json gzipfile : ' + self.jsonZipFile)
+    jsonZipfile = f'{self.jobId}.{self.jmeta.fileExt}'
+    logger.info('output json gzipfile : ' + jsonZipfile)
 
-    zipFilePath = '%s/%s' % (self.repoPath, self.jsonZipFile)
-    dstream = self.getFileStream()
-
-    try:
-      with open(zipFilePath, 'wb') as fhwb:
-        for chunk in dstream.iter_content(chunk_size=1024): 
-          if chunk: # filter out keep-alive new chunks
-            fhwb.write(chunk)
-    except Exception as ex:
-      errmsg = '%s write failed : ' + self.jsonZipFile
-      logger.error(errmsg)
-      raise
-
-  # -------------------------------------------------------------- #
-  # getFileStream
-  # ---------------------------------------------------------------#
-  def getFileStream(self):
-    try:
-      dstream = self._getFileStream()
-    except Exception as ex:
-      errmsg = 'json gzip file stream api request failed'
-      logger.error(errmsg)
-      raise
-    if dstream.status_code != 201:
-      errmsg = 'json gzip file stream api request failed\nError : %s' % dstream.text
-      raise Exception(errmsg)
-    return dstream
-
-  # -------------------------------------------------------------- #
-  # _getFileStream
-  # ---------------------------------------------------------------#
-  def _getFileStream(self):
-    packet = {'jobId':self.jobId,'service':'datatxn.dataTxnPrvdr:BinryFileStreamPrvdr'}
-    packet['responseType'] = 'stream'
-    packet['args'] = [self.jobId, self.jsonZipFile]
-    apiUrl = 'http://%s/api/v1/sync' % self.hostName
-    # NOTE: stream=True parameter
-    return self.request.post(apiUrl,stream=True,json={'job':packet})
-
-  # -------------------------------------------------------------- #
-  # uncompressFile
-  # ---------------------------------------------------------------#
-  def uncompressFile(self):
-    logger.info('%s, gunzip ...' % self.jsonZipFile)
-    try:
-      cmdArgs = ['gunzip','-f',self.jsonZipFile]
-      self.sysCmd(cmdArgs,cwd=self.repoPath)
-    except Exception as ex:
-      errmsg = '%s, gunzip failed' % self.jsonZipFile
-      logger.error(errmsg)
-      raise
-
+    dbKey = f'{self.jobId}|datastream|workspace'
+    self._leveldb[dbKey] = repoPath
+    dbKey = f'{self.jobId}|datastream|outfile'
+    self._leveldb[dbKey] = jsonZipfile
