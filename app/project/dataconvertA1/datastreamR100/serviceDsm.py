3fc99aaf7f50b70766771c63ed7a5e9b18959781
Peter McGill <gilletahom5@gmail.com>
2020-08-05 13:31:34 +1000

    Update serviceDsm.py

--- app/project/dataconvertA1/datastreamR100/serviceDsm.py
+++ app/project/dataconvertA1/datastreamR100/serviceDsm.py
@@ -13,6 +13,8 @@ class ServiceA(AbstractTxnHost):
     super().__init__(connector, contextId)
     self.actorId = actorId
     self.__dict__['NOTIFY'] = self._NOTIFY
+    self.__dict__['PREPARE'] = self._PREPARE
+    self.__dict__['START'] = self._START
 
   #----------------------------------------------------------------#
   # sockAddr
@@ -22,10 +24,7 @@ class ServiceA(AbstractTxnHost):
     return self._conn.sockAddr
 
   def __getitem__(self, request):
-    try:
-      return self._conn[request]
-    except KeyError:
-      return getattr(self, request)
+    return getattr(self, request)
 
   #----------------------------------------------------------------#
   # make - replaced zmq.DEALER with zmq.ROUTER to avoid confusion.
@@ -57,3 +56,43 @@ class ServiceA(AbstractTxnHost):
     logger.info(f'{self.hostname}, {jobId} notified ...')
     self.active.clear()
     asyncio.sleep(0.1)
+    
+  #----------------------------------------------------------------#
+  # _PREPARE - moved from connectorDsm.DatastreamResponse, to improve 
+  # datastream service concept presentation
+  #----------------------------------------------------------------#		
+  async def _PREPARE(self, jobId, taskId):
+    self.jobId = jobId
+    logger.info(f'{self.name}, job {jobId}, preparing {taskId} data stream ...')    
+    hardhash = HardhashContext.connector(jobId)
+    try:
+      dbKey = f'{jobId}|workspace'
+      workspace = hardhash[dbKey]
+      dbKey = f'{jobId}|datastream|infile'
+      self.infileName = hardhash[dbKey]
+    except KeyError as ex:
+      errmsg = f'{jobId}, failed to get job article from datastorage'
+      await self.sock.send_json([500, {'error': errmsg}])
+      raise TaskError(errmsg)
+
+    logger.info(f'{self.name}, datastream workspace, infile : {workspace}, {self.infileName}')
+    self.infilePath = f'{workspace}/{self.infileName}'
+    if not os.path.exists(self.infilePath):
+      errmsg = f'source file {self.infileName} does not exist in workspace'
+      await self.sock.send_json([500, {'error': errmsg}])
+    else:
+      await self.sock.send_json([200, {'status':'ready','infile':f'{self.infileName}'}])
+
+  #----------------------------------------------------------------#
+  # _START - moved from connectorDsm.DatastreamResponse, to improve
+  # datastream service concept presentation
+  #----------------------------------------------------------------#		
+  async def _START(self):
+    logger.info(f'{self.name}, job {self.jobId}, now streaming file {self.infileName} ...')
+    with open(self.infilePath, "rb") as bfh:
+      while True:
+        chunk = bfh.read(1024)
+        if not chunk:
+          await self.sock.send(b'')
+          break
+        await self.sock.send(chunk,flags=zmq.SNDMORE)
